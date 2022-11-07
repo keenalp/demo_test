8e9033a6673206300de7bc97c2bc4bc63223d6b4
keenalp <115138521+keenalp@users.noreply.github.com>
2022-11-07 09:56:05 +0530

    testing features of git

--- data.js
+++ data.js
@@ -2,35 +2,35 @@ export default [
   {
     id: 1,
     name: 'Bertie patel',
-    age: 20,
+    age: 21,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
     name: 'Hester patil',
-    age: 31,
+    age: 3,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
     name: 'Larry patil',
-    age: 34,
+    age: 4,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
     name: 'Sean patel',
-    age: 45,
+    age: 4,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
     name: 'Lola patel',
-    age: 30,
+    age: 3,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
